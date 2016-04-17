ce9e6088614b5e032c32349a0e6fe3cc187dfa88
Francesc Ramírez <francescramirez@gmail.com>
2016-04-17 20:34:53 +0200

    add setup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  console.log('All tasks done...');
-})
+gulp.task('setup', function(){
+  var envFile = 'DB_HOST=mongodb://localhost:27017/kapeloi\nPORT=8000\nSECRET=secretPass';
+  require('fs').writeFileSync('./.env', envFile);
+});
