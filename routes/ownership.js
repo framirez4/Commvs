4b8b99d10ddc9efe40ec0a71bb7726eb8d30f068
Francesc Ramírez <francescramirez@gmail.com>
2016-06-15 18:23:37 +0200

    Change permission on GET ownership

--- routes/ownership.js
+++ routes/ownership.js
@@ -10,7 +10,7 @@ var router          = require('express').Router(); // Create router
 
 //ROUTES for OWNERSHIPS
 router.route('/:comm_id')
-  .get(authController.verifyToken, user.can('run admin-only functions'), commsController.getOwnership) // Admin - generate an ownership code
+  .get(authController.verifyToken, user.can('update an owned comm data'), commsController.getOwnership) // Admin - generate an ownership code
   .post(authController.verifyToken, commsController.setOwnership)   // Allowed to every identified user, since it uses the logged account
   .delete(authController.verifyToken, commsController.removeOwnership);  // Allowed to every identified user, since it uses the logged account
 module.exports = router;
