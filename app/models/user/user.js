bac4aa2023212c5fb0fd87d04888c09a7cb87011
Francesc Ramírez <francescramirez@gmail.com>
2017-03-08 20:56:01 +0100

    change bcrypt for bcryptjs

--- app/models/user/user.js
+++ app/models/user/user.js
@@ -1,7 +1,7 @@
 // Load required packages
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const utils = require('../../controllers/utils')
 const saltRounds = 10
 
